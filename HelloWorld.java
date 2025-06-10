53f013428aa587ade5eac4913b0648a368b319d6
@ HAIDER. <abbaswafa356@gmail.com>
2025-06-10 00:09:59 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,32 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        String k= "ali";
-        System.out.println(k + " is back");
-        Integer n1=7;
-        Integer n2=7;
-        String n3="7";
-        System.out.println(n1 + " Ali");
-        System.out.println(n1+k);
-        System.out.println(n1+n2);
-        System.out.println(n1+n3);
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i);
-        }
+
+    }
     }
 }
+//Single Line Comment :
+//we use double slash(//) for a single line comment.
+
+//Double line comment:
+// we use single slash and a start for a double line comment e.g:
+
+/*my name is ....
+ * my age is....
+ * i am from....
+ */
+
+/* hum class ya function ka name rakhne ke leye  2 tarah ki 
+convention use karte hein or yaad rahe space nhi use hotti
+1: Pascal Convention : ye class ka name rakhne ke leye use hotti he.
+    is mein har word ka first letter capital hotta he . e.g:
+    .HelloWorld
+    .MyClass
+    .CarManager
+
+2: Camel Convention: ye funtion ya variable ka name rakhne ke leye
+    is convention ko use kare hein. iss mein first word ka first
+    letter smaal baki aagle wale words ka first letter big hotta he
+    e.g:
+     .main
+     .classManager
