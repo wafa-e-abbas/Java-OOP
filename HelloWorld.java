d33c96902b1cc1808b4c9a958e3defd8a1d94e23
@ HAIDER. <abbaswafa356@gmail.com>
2025-06-10 00:07:45 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String k= "ali";
+        System.out.println(k + " is back");
+        Integer n1=7;
+        Integer n2=7;
+        String n3="7";
+        System.out.println(n1 + " Ali");
+        System.out.println(n1+k);
+        System.out.println(n1+n2);
+        System.out.println(n1+n3);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
